f03363310a351bfab908d00d1b0fbc981806487f
Aravind <g.aravind4@gmail.com>
2018-05-20 22:42:20 +0530

    All fixed

--- database/events.sql
+++ database/events.sql
@@ -1,9 +1,10 @@
-USE blockb;
+USE vote;
 
 SET GLOBAL event_scheduler = ON;
 
 CREATE EVENT expirePolls
   ON SCHEDULE EVERY 30 second
   DO UPDATE polls SET pollExpired = true
-  WHERE (pollTimeEnd < now())
+  WHERE (pollTimeEnd < now()-interval 330 minute)
   AND pollExpired IS NULL;  
+  
